e21a5b37afa2ead406bdc76c7903b0e1fc18e2d1
Fabien Costa <fabiencosta94800@gmail.com>
2024-07-04 02:02:39 +0200

    param react

---
+++ frontend/src/components/header/Header.jsx
@@ -0,0 +1,28 @@
+import "./header.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircle as faCircleUse } from "@fortawesome/free-solid-svg-icons";
+
+const Header = () => {
+  return (
+    <div>
+      <nav className="main-nav">
+        <a className="main-nav-logo" href="./index.html">
+          <img
+            className="main-nav-logo-image"
+            src="src\assets\images\argentBankLogo.png"
+            alt="Argent Bank Logo"
+          />
+        </a>
+        <div>
+          <a className="main-nav-item" href="./sign-in.html">
+            <FontAwesomeIcon icon={faCircleUse} />
+            <i className="fa fa-user-circle">lala</i>
+            Sign In
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export { Header };
