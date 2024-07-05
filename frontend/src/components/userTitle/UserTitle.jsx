4662594632ab21efc64e4e92113dbc9bb09216bb
Fabien Costa <fabiencosta94800@gmail.com>
2024-07-05 16:17:29 +0200

    add redux pluggin + html/css

---
+++ frontend/src/components/userTitle/UserTitle.jsx
@@ -0,0 +1,16 @@
+const UserTitle = () => {
+  return (
+    <div>
+      <div className="header">
+        <h1>
+          Welcome back
+          <br />
+          Tony Jarvis!
+        </h1>
+        <button className="edit-button">Edit Name</button>
+      </div>
+    </div>
+  );
+};
+
+export { UserTitle };
