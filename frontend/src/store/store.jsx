0154ceb315588f8b2c884b4f11d770d9baf0963e
Fabien Costa <fabiencosta94800@gmail.com>
2024-07-19 14:37:40 +0200

    try to keep session alive

--- frontend/src/store/store.jsx
+++ frontend/src/store/store.jsx
@@ -1,8 +1,10 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import { loginReducer } from "./reducers/reducers";
+import { userReducer } from "./reducers/userReducer";
 
 const rootReducer = combineReducers({
   login: loginReducer,
+  user: userReducer,
 });
 
 export const store = configureStore({
